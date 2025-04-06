69769c3dc493665abc1e57e00450ca04bc8bcbcc
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2025-04-06 12:46:45 +0530

    Add files via upload

---
+++ backend/models/user.model.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minlength: [ 3, 'First name must be at least 3 characters long' ],
+        },
+        lastname: {
+            type: String,
+            minlength: [ 3, 'Last name must be at least 3 characters long' ],
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [ 5, 'Email must be at least 5 characters long' ],
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,      // when we are finding the user, their password should not be visible
+    },
+    // this will be used for live tracking of the captain
+    socketId: {
+        type: String,
+    },
+})
+
+// generating authentication token
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+    return token;
+}
+
+// comparing the password in the database and what user enters while log in
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+// hashing the password to keep it safe. Then, we will store it in database.
+userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+}
+
+const userModel = mongoose.model('user', userSchema);
+
+
+module.exports = userModel;
